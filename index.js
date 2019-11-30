e8438153a7aaa294355b1b31462b385eca5303b2
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 04:51:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function takeANumber(currentLine,newPersonName){
-return "Welcome, " + newPersonName +". You are number " + currentLine + " in line."
+return `Welcome, $(newPersonName). You are number $(currentLine+1) in line.`
 }
