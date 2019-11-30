9e744ac5488101105d7e56d13fa5eb50873c8d5f
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 05:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ return `Welcome, ${newPersonName}. You are number ${katzDeli.length} in line.`
 }
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine[1]}`
 
   }
   else{
