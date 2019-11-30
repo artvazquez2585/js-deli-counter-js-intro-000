1a8d24eb6e147bcce1f13dd58857c589a63607f9
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 04:59:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function takeANumber(currentLine,newPersonName){
-return `Welcome, ${newPersonName}. You are number ${currentLine+1} in line.`
+katzDeliLine.push = newPersonName;
+return `Welcome, ${newPersonName}. You are number ${currentLine.length+1} in line.`
 }
