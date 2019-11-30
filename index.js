20d61ed53b9b0e987b382045b03ca745f8bb9ebb
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 05:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length > 0){
     let message = "The line is currently:"
     for(let i=0, i < katzDeliLine.length, i++){
-      message = `${message} ${i+1}.${katzDeliLine[i],}`
+      let message = `${message} ${i+1}.${katzDeliLine[i],}`
     }
     return message
   }
